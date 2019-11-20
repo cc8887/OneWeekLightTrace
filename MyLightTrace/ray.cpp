eb58f27ed4f389d9d9940932466f5f598ecf2bd6
cc8887 <qq727626166@Gmail.com>
2019-11-20 15:55:58 +0800

    1. 完成对ray中函数的重构，将所有源码都放入cpp 2. 将源文件进行进一步整理

--- MyLightTrace/ray.cpp
+++ MyLightTrace/ray.cpp
@@ -5,8 +5,14 @@
 ray::ray()
 {
 }
-
+ray::ray(const vec3& a,const vec3& b){
+	A = a;
+	B = b;
+}
 
 ray::~ray()
 {
 }
+vec3 ray::origin() const { return A; }
+vec3 ray::direction() const { return B; }
+vec3 ray::point_at_parameter(float t) const { return A + t * B; }
--- MyLightTrace/ray.h
+++ MyLightTrace/ray.h
@@ -9,16 +9,12 @@ class ray
 public:
 	
 	ray();
-	ray(const vec3& a, const vec3& b) {
-		A = a;
-		B = b;
-	}
-	
+	ray(const vec3& a, const vec3& b);
 	~ray();
 
-	vec3 origin() const{ return A; }
-	vec3 direction() const { return B; }
-	vec3 point_at_parameter(float t) const { return A + t * B; }
+	vec3 origin() const;
+	vec3 direction() const;
+	vec3 point_at_parameter(float t) const;
 	vec3 A;
 	vec3 B;
 };
